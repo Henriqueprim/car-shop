242ed11531ca1eaca4129d20da1d9796c2de850c
Henriqueprim <henrique_salmprim@hotmail.com>
2022-12-29 20:43:57 -0300

    feat: CarService with create function

--- src/Services/CarService.ts
+++ src/Services/CarService.ts
@@ -1,5 +1,21 @@
-class CarService {
+import Car from '../Domains/Car';
+import ICar from '../Interfaces/ICar';
+import CarODM from '../Models/CarODM';
 
+class CarService {
+  model = new CarODM();
+
+  private carDomain(car: ICar | null): Car | null {
+    if (car) {
+      return new Car(car);
+    }
+    return null;
+  }
+
+  public async create(car: ICar) {
+    const createdCar = await this.model.create(car);
+    return new Car(createdCar);
+  }
 }
 
 export default CarService;
