4e2b6c31c58f3d26fcf51a67303199b0c2ddac39
Henriqueprim <henrique_salmprim@hotmail.com>
2022-12-29 16:57:28 -0300

    test: createCar test

---
+++ tests/unit/Services/CarService.test.ts
@@ -0,0 +1,29 @@
+import { Model } from 'mongoose';
+import Sinon from 'sinon';
+import { expect } from 'chai';
+import Car from '../../../src/Domains/Car';
+import ICar from '../../../src/Interfaces/ICar';
+
+describe('Tests /cars services', function () {
+  it('Should create a car successfully', async function () {
+    const carInput: ICar = {
+      model: 'Marea',
+      year: 2002,
+      color: 'Black',
+      status: true,
+      buyValue: 15.990,
+      doorsQty: 4,
+      seatsQty: 5,
+    };
+    const carOutput: Car = new Car({
+      ...carInput,
+      id: '53468fafae523468',
+    });
+    Sinon.stub(Model, 'create').resolves(carOutput);
+
+    const service = new CarService();
+    const result = await service.create(carInput);
+
+    expect(result).to.be.deep.equal(carOutput);
+  });
+});
