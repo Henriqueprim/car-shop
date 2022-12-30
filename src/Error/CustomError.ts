2ebd49892d0c1a76f7651ce11f01a3304751952f
Henriqueprim <henrique_salmprim@hotmail.com>
2022-12-30 18:26:38 -0300

    feat: CustomError

---
+++ src/Error/CustomError.ts
@@ -0,0 +1,9 @@
+class CustomError extends Error {
+  public status: number;
+  constructor(message: string, status: number) {
+    super(message);
+    this.status = status;
+  }
+}
+
+export default CustomError;
