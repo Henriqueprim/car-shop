186ea549c2c33eb510ff5026632bfefb39a151ea
Henriqueprim <henrique_salmprim@hotmail.com>
2022-12-30 17:57:02 -0300

    feat: CarController findById function

--- src/Controllers/CarController.ts
+++ src/Controllers/CarController.ts
@@ -42,6 +42,16 @@ class CarController {
       this.next(error);
     }
   }
+
+  public async findById() {
+    const { id } = this.req.params;
+    try {
+      const car = await this.service.findById(id);
+      return this.res.status(200).json(car);
+    } catch (error) {
+      this.next(error);
+    }
+  }
 }
 
 export default CarController;
