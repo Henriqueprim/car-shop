6778473d991a392b8aea6d0e515c7fc4c768cb43
Henriqueprim <henrique_salmprim@hotmail.com>
2022-12-31 14:39:01 -0300

    feat: implements update in motorcycle controller

--- src/Controllers/MotorcycleController.ts
+++ src/Controllers/MotorcycleController.ts
@@ -51,6 +51,17 @@ class MotorcycleController {
       this.next(error);
     }
   }
+
+  public async update() {
+    const { id } = this.req.params;
+    const { body } = this.req;
+    try {
+      const updatedBike = await this.service.update(id, body);
+      return this.res.status(200).json(updatedBike);
+    } catch (error) {
+      this.next(error);
+    }
+  }
 }
 
 export default MotorcycleController;
