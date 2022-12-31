0110ab7f0912c4799d7ffa714109fd493fecdf4c
Henriqueprim <henrique_salmprim@hotmail.com>
2022-12-31 14:14:38 -0300

    test: Motorcycle getAll function test

--- tests/unit/Services/MotorcycleService.test.ts
+++ tests/unit/Services/MotorcycleService.test.ts
@@ -29,4 +29,33 @@ describe('Tests /motorcycles services', function () {
     const result = await service.create(bikeInput);
     expect(result).to.be.deep.equal(bikeOutput);
   });
+
+  it('Should get the list of motorcycles in the database', async function () {
+    const bikeListMock: IMotorcyle[] = [
+      {
+        id: '634852326b35b59438fbea2f',
+        model: 'Honda Cb 600f Hornet',
+        year: 2005,
+        color: 'Yellow',
+        status: true,
+        buyValue: 30.000,
+        category: 'Street',
+        engineCapacity: 600,
+      },
+      {
+        id: '634852326b35b59438fbea31',
+        model: 'Honda Cbr 1000rr',
+        year: 2011,
+        color: 'Orange',
+        status: true,
+        buyValue: 59.900,
+        category: 'Street',
+        engineCapacity: 1000,
+      },
+    ];
+    const bikeListOutput: Motorcycle[] = bikeListMock.map((bike) => new Motorcycle(bike));
+    Sinon.stub(Model, 'find').resolves(bikeListOutput);
+    const result = await service.getAll();
+    expect(result).to.be.deep.equal(bikeListOutput);
+  });
 });
